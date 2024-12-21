53b3b8a05217b4f9616b0b8b526fcdd3d62f2d90
Lannion <jared8921@gmail.com>
2024-12-21 14:41:30 +0800

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!++@ql*i^zgm9rgz8hhxddhz$%53vqj*p(dkv5c437z*$urfu+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
